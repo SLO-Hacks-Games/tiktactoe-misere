341f4d34667d388d3779a00445ff83311cf152a7
Zhi He <zhihe122@gmail.com>
2022-04-09 15:13:31 -0700

    finished some more function

--- src/Board.java
+++ src/Board.java
@@ -3,21 +3,29 @@ public class Board {
 
     public void checkMore(int row, int col) {
         if (this.checkforzero(row, col)) {
-            for (int r = row - 2; r < 3; r++) {
-                for (int c = col - 2; c < 3; c++) {
-                    if (r < 0 || c < 0 || r > matrix[1].length || c > matrix[0].length) {
-                        continue;
-                    }
+            matrix[row][col] += 1;
 
-                }
-            }
         }
     }
     public boolean checkforzero(int row, int col) {
         return matrix[row][col] == 0;
     }
     public void updateBoardSize() {
-        matrix = new int[matrix[0].length + 1][matrix[1].length + 1];
+        matrix = new int[matrix[0].length + 1][matrix.length + 1];
     }
 
+    public boolean checkFull() {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                if (matrix[r][c] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int getWidth() {
+        return matrix[0].length;
+    }
 }
