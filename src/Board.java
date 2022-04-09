a44325396de7978458897c7c2684a81595a41090
Zhi He <zhihe122@gmail.com>
2022-04-09 14:43:17 -0700

    finished logic

--- src/Board.java
+++ src/Board.java
@@ -2,7 +2,16 @@ public class Board {
     private int [][] matrix = new int[3][3];
 
     public void checkMore(int row, int col) {
+        if (this.checkforzero(row, col)) {
+            for (int r = row - 2; r < 3; r++) {
+                for (int c = col - 2; c < 3; c++) {
+                    if (r < 0 || c < 0 || r > matrix[1].length || c > matrix[0].length) {
+                        continue;
+                    }
 
+                }
+            }
+        }
     }
     public boolean checkforzero(int row, int col) {
         return matrix[row][col] == 0;
