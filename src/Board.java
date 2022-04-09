f272da72f12e0c13cb2e284bcc0290a5a4604112
Zhi He <zhihe122@gmail.com>
2022-04-09 14:26:06 -0700

    finished 2 board function

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,14 @@
 public class Board {
+    private int [][] matrix = new int[3][3];
+
+    public void checkMore(int row, int col) {
+
+    }
+    public boolean checkforzero(int row, int col) {
+        return matrix[row][col] == 0;
+    }
+    public void updateBoardSize() {
+        matrix = new int[matrix[0].length + 1][matrix[1].length + 1];
+    }
+
 }
